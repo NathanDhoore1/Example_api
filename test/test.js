60b0bea027c3e4605c1945deb24bb78378448d04
Nathan D'hoore <r0916741@student.thomasmore.be>
2022-11-25 13:21:40 +0100

    Test

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const request= require('supertest');
+
+describe('get campus', () => {
+    it('should return first campus', async () => {
+    await request('https://nathandhoore.netlify.app')
+    .get('/.netlify/functions/api/campus')
+    .expect(200)
+    .expect('Content-Type', 'application/json; charset=utf-8')
+    .expect((res) => {
+        console.log(res.body[0])
+    })
+});
+});
+
+
